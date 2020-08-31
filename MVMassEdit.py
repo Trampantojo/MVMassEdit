7b6d195e293a9e967f9921e8ed61b8cfe701e04e
Un Trampantojo <redneboa@gmail.com>
2020-08-31 22:51:28 +0200

    [IMP] Refactor and new options (file, max-tries, ...)

--- MVMassEdit.py
+++ MVMassEdit.py
@@ -4,7 +4,7 @@
 # -----
 # Para usarse se necesita obtener el hash de sesión del usuario.
 # Esta cookie usa "HttpOnly" y para obtener el valor se tiene que abrir
-# el panel de 'DevTools' pulsado CTRL+SHIFT+I e ir a la
+# el panel de 'DevTools' del navegador pulsando CTRL+SHIFT+I e ir a la
 # pestaña "Application" (Chrome) o "Storage" (Firefox), allí ir al
 # apartado "Cookies" y copiar el valor de la cookie que se llama "sess".
 # -----
@@ -15,8 +15,9 @@ import time
 import queue
 import asyncio
 import sys
+import json
 from requests.exceptions import ConnectionError
-from typing import List, Tuple
+from typing import List, Tuple, Callable, Awaitable, Any
 from lxml import etree
 from io import StringIO
 from cssselect import HTMLTranslator, SelectorError
@@ -29,6 +30,12 @@ logging.basicConfig(level=logging.NOTSET,
 logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
 
 
+TEXT_SECONDS = {
+    'segundos': 1,
+    'minutos': 60,
+    'horas': 3600
+}
+
 def css_xpath(tree: etree.ElementTree, css_selector: str) -> List[etree.Element]:
     """Helper function to get lxml elements using css selectors"""
     try:
@@ -38,20 +45,21 @@ def css_xpath(tree: etree.ElementTree, css_selector: str) -> List[etree.Element]
     return tree.xpath(expression)
 
 def get_post_url_values(url: str) -> Tuple[int, int, int]:
-    """Helper function to get information of the post from the url"""
+    """Helper function to get information of the post from the url
+    Tuple result is: TID, NUM, PAG
+    """
     g = re.search(r'(\d+)/(\d+)#(\d+)', url)
     if g is None:
         g = re.search(r'(\d+)#(\d+)', url)
-        (tid, num, pag) = (g[1], g[2], 1)
-    else:
-        (tid, num, pag) = (g[1], g[3], g[2])
-    return (tid, num, pag)
+        return (int(g[1]), int(g[2]), 1)
+    return (int(g[1]), int(g[3]), int(g[2]))
 
 class MVHttp(object):
     _protocol = 'https://'
     _domain = 'www.mediavida.com'
 
-    def __init__(self, session_hash: str):
+    def __init__(self, session_hash: str, max_tries: int = 3):
+        self._max_tries = max_tries
         self._requests = requests.Session()
         self._htmlparser = etree.HTMLParser()
         session_cookie = {
@@ -72,29 +80,34 @@ class MVHttp(object):
         self._requests.cookies.set(**session_cookie)
 
     def _get(self, url: str, *args: str, **kwargs: int) -> etree.ElementTree:
+        """Send a HTTP Get request"""
         r = self._requests.get(f"{self._protocol}{self._domain}/{url}", *args, **kwargs)
         if r.status_code == 200:
             return etree.parse(StringIO(r.text), self._htmlparser)
         return None
 
     def _post(self, url: str, *args: str, **kwargs: int) -> bool:
-        kwargs['allow_redirects'] = False
+        """Send a HTTP Post request
+        All is done successfully if the server redirect to the edited post
+        """
         r = self._requests.post(f"{self._protocol}{self._domain}/{url}", *args, **kwargs)
-        return r.status_code == 200
+        return r.status_code == 200 and '#' in r.url
 
-    def _check_errors(self, tree: etree.ElementTree) -> None:
+    def _check_errors(self, tree: etree.ElementTree) -> bool:
+        """
+        MediaVida uses a rule to avoid requests flood. This method analyzes 
+        the response to be compliant with this rule, this can block the 
+        program execution for a long time.
+        """
         error_elem = css_xpath(tree, "#errorbox li")
         if len(error_elem):
             g = re.search(r'Espera\s(\d+)\s(\w+)', error_elem[0].text)
-            seconds_map = {
-                'segundos': 1,
-                'minutos': 60,
-                'horas': 3600
-            }
             if g is not None:
-                segs = int(g[1]) * seconds_map[g[2]] + 1
+                segs = int(g[1]) * TEXT_SECONDS[g[2]] + 1
                 logging.info(f"Needs wait {segs} seconds to continue...")
                 time.sleep(segs)
+                return True
+        return False
 
     def get_user_posts(self, user: str) -> List[str]:
         """Gets all the user posts meta-data"""
@@ -112,41 +125,57 @@ class MVHttp(object):
             posts += [link.get('href') for link in links]
         return posts
 
-    async def edit_post(self, url: str, message: str) -> Tuple[str, bool]:
-        """
-        Try edit the given post. 
-        MediaVida uses a rule to avoid requests flood. This method analyzes 
-        the response to be compliant with this rule, this can block the 
-        program execution for a long time.
-        """
+    async def edit_post(self, url: str, message: str, _try_count: int = 1) -> Tuple[str, bool]:
+        """Try edit the given post."""
+        
+        async def try_again():
+            """Try edit the page again"""
+            if _try_count == self._max_tries:
+                logging.error("Max-tries reached! can't edit the post.")
+                return (url, False)  
+            try_count_n = _try_count + 1
+            logging.info(f"Trying to edit the post again ({try_count_n})...")
+            return await self.edit_post(url, message, _try_count=try_count_n)
+
         logging.info(f"Editing '{url}'...")
         (tid, num, pag) = get_post_url_values(url)
         try:
             tree = self._get(f"foro/post.php?tid={tid}&num={num}&pagina={pag}")
             if not tree:
-                return False
+                return (url, False)
             current_message = css_xpath(tree, "#cuerpo")[0].text
             if current_message == message:
-                logging.info("The post is already edited. Omitting.")
+                logging.info("The post has already been edited. Omitting!")
                 return (url, True)
-            self._check_errors(tree)
+            if self._check_errors(tree):
+                return await try_again()
             token = css_xpath(tree, "#token")[0].get('value')
             fid = css_xpath(tree, "#fid")[0].get('value')
-            return (url, self._post("foro/action/poster.php", data={
+            if not self._post("foro/action/poster.php", data={
                 'cuerpo': message,
                 'token': token,
                 'fid': fid,
                 'tid': tid,
                 'num': num,
                 'pagina': pag
-            }))
+            }):
+                return await try_again()
         except ConnectionError:
             logging.error("Failed! Can't edit the post")
-            return (url, False)
+            return await try_again()
+        return (url, True)
 
-class MultiEditRequest(object):
-    def __init__(self, mvhttp: MVHttp):
-        self._mvhttp = mvhttp
+class MultiRequestTask(object):
+    def __init__(self, url: str, call_ref: Callable[..., Awaitable[Any]], call_args: List[Any]):
+        self._url = url
+        self._call_ref = call_ref
+        self._call_args = call_args
+
+    async def run(self) -> Callable[..., Awaitable[Any]]:
+        return await self._call_ref(self._url, *self._call_args)
+
+class AsyncMultiRequest(object):
+    def __init__(self):
         self.reset()
 
     def jobs(self) -> int:
@@ -159,55 +188,71 @@ class MultiEditRequest(object):
         self._fails = []
         self._queue_out = queue.SimpleQueue()
 
-    def request_q(self, url: str, message: str) -> None:
-        self._queue_out.put((url, message))
+    def request_q(self, task: MultiRequestTask) -> None:
+        self._queue_out.put(task)
 
     def process_queue(self, amount: int) -> None:
         loop = asyncio.get_event_loop()
         tasks = []
         for _ in range(amount):
             if not self._queue_out.empty():
-                (url, message) = self._queue_out.get()
-                tasks.append(self._mvhttp.edit_post(url, message))
+                task = self._queue_out.get()
+                tasks.append(task.run())
         (done, _) = loop.run_until_complete(asyncio.wait(tasks))
-        # TODO: Add support to max-tries
         for fut in done:
             (url, res) = fut.result()
             if not res:
                 self._fails.append(url)
 
 class MVMassEdit(object):
-    # FIXME: Improve to use multi-requests.
-    # MV uses a window time anti-flood feature.
-    _max_requests = 1
+    def __init__(self, session_hash: str, max_tries: int):
+        self._mvhttp = MVHttp(session_hash, max_tries)
+        self._multi_edit = AsyncMultiRequest()
 
-    def __init__(self, session_hash: str, omit_first: bool):
-        self._omit_first = omit_first
-        self._mvhttp = MVHttp(session_hash)
-        self._multi_edit = MultiEditRequest(self._mvhttp)
-
-    def prepare(self, user: str, message: str) -> None:
-        posts = self._mvhttp.get_user_posts(user)
-        logging.info(f"Found {len(posts)} possible posts to edit...")
-        self._multi_edit.reset()
-        posts.reverse()
-        for url_post in posts:
-            (_, _, num) = get_post_url_values(url_post)
-            if num == 1 and self._omit_first:
-                continue
-            self._multi_edit.request_q(url_post, message)
-
-    def run_loop(self, delay: int) -> None:
-        fails = []
-        loop = asyncio.get_event_loop()
-        while self._multi_edit.jobs():
-            self._multi_edit.process_queue(self._max_requests)
-            time.sleep(delay)
-        loop.close()
+    def _process_fails(self) -> None:
         fails = self._multi_edit.fails()
         if any(fails):
             logging.warning(f"{len(fails)} posts can't be edited!")
             logging.debug(fails)
+        with open('fails.json', 'w') as json_file:
+            json.dump(fails, json_file)
+    
+    def _add_requests_q(self, posts: List[str], message: str, omit_first: bool) -> int:
+        for url_post in posts:
+            (_, _, num) = get_post_url_values(url_post)
+            if num == 1 and omit_first:
+                continue
+            self._multi_edit.request_q(MultiRequestTask(url_post, self._mvhttp.edit_post, (message,)))
+        return self._multi_edit.jobs()
+
+    def prepare_from_file(self, filepath: str, message: str, omit_first: bool) -> bool:
+        with open(filepath) as f:
+            posts = json.load(f)
+        if not any(posts):
+            return False
+        self._multi_edit.reset()
+        jobs_count = self._add_requests_q(posts, message, omit_first)
+        logging.info(f"Found {jobs_count} possible posts to edit...")
+        return True
+
+    def prepare(self, user: str, message: str, omit_first: bool) -> bool:
+        posts = self._mvhttp.get_user_posts(user)
+        if not any(posts):
+            return False
+        self._multi_edit.reset()
+        posts.reverse() # Oldest first
+        jobs_count = self._add_requests_q(posts, message, omit_first)
+        logging.info(f"Found {jobs_count} possible posts to edit...")
+        return True
+
+    def run_loop(self, requests_amount: int, delay: int) -> None:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        while self._multi_edit.jobs():
+            self._multi_edit.process_queue(requests_amount)
+            time.sleep(delay)
+        loop.close()
+        self._process_fails()
 
 
 if __name__ == '__main__':
@@ -216,7 +261,6 @@ if __name__ == '__main__':
         add_help=True)
     parser.add_argument(
         '-u', '--user',
-        required=True,
         help='User login')
     parser.add_argument(
         '-t', '--token',
@@ -230,6 +274,17 @@ if __name__ == '__main__':
         default=2,
         help='Multi-request delay')
     parser.add_argument(
+        '-ra', '--requests-amount',
+        default=3,
+        help='Multi-request amount')
+    parser.add_argument(
+        '-mt', '--max-tries',
+        default=3,
+        help="Amount of requests before abort the 'post' edition")
+    parser.add_argument(
+        '-f', '--file',
+        help='Edit the messages declared in the file')
+    parser.add_argument(
         '--omit-first',
         action='store_true',
         help='Omit the first post of the thread')
@@ -240,6 +295,16 @@ if __name__ == '__main__':
         logging.error('No session token given. Aborting!')
         exit(-1)
 
-    mvmassedit = MVMassEdit(args.token, args.omit_first)
-    mvmassedit.prepare(args.user, args.message)
-    mvmassedit.run_loop(args.delay)
+    mvmassedit = MVMassEdit(args.token, args.max_tries)
+    if args.file:
+        if not mvmassedit.prepare_from_file(args.file, args.message, args.omit_first):
+            logging.error("Can't load any post from the file. Aborting!")
+            exit(-1)
+    else:
+        if not args.user:
+            logging.error('No user given. Aborting!')
+            exit(-1)
+        elif not mvmassedit.prepare(args.user, args.message, args.omit_first):
+            logging.error("Can't load any post from the user. Aborting!")
+            exit(-1)
+    mvmassedit.run_loop(args.requests_amount, args.delay)
--- README.md
+++ README.md
@@ -8,29 +8,43 @@
 | Corto | Largo | Descripción | Por defecto | Requerido |
 |-------|-------|-------------|-------------|-----------|
 | -h | --help | Muestra información | | No |
-| -u | --user | Usuario | | Sí |
-| -t | --token | Hash de sesión | | Sí** |
+| -u | --user | Usuario | | Sí<sup>1</sup> |
+| -t | --token | Hash de sesión | | Sí<sup>2</sup> |
 | -m | --message | El nuevo mensaje ha escribir | . | No |
+| -f | --file | Edita los posts declarados en el archivo | | No |
+| -ra | --requests-amount | Cantidad de peteciones simultaneas | 3 | No |
+| -mt | --max-tries | Numero de intentos antes de abortar la edición del mensaje | 3 | No |
 | -d | --delay | Tiempo (en segundos) entre que se envían las peticiones | 2 | No |
-|    | --omit-first | Omite la edición del post principal de un hilo | False | No |
+| | --omit-first | Omite la edición del post principal de un hilo | False | No |
 
-** El hash de sesión será solicitado por el script si no se especifica como parámetro de entrada (-t|--token).
-** Se recomienda dejar que el script solicite el hash de sesion para evitar que se quede almacenado en el historico del terminal.
+<font size="2">1. El parámetro es obligatorio si no se usa el parámetro ```-f|--file```.
+2. El hash de sesión será solicitado por el script si no se especifica como parámetro de entrada ```-t|--token```. Recomiendo dejar que el script solicite el hash de sesion para evitar que se quede almacenado en el historico del terminal.</font>
+
+** Recomiendo no tocar el parametro ```-ra|--requests-amount```. Debido a los mecanismos de la web de mediavida, incrementar la cantidad de peticiones solo hará que el script vaya más lento.
+
 
 ## Instalación
 
 Puedes usar "pip" para instalar todo lo necesario para que el script funcione:
+
 ```pip install -r requirements.txt```
+
 
 ## Uso
 
 Para usarse se necesita obtener el hash de sesión del usuario.
-Esta cookie usa "HttpOnly" y para obtener el valor se tiene que abrir el panel de 'DevTools' pulsado CTRL+SHIFT+I e ir a la pestaña "Application" (Chrome) o "Storage" (Firefox), allí ir al apartado "Cookies" y copiar el valor de la cookie que se llama "sess".
+Esta cookie usa "HttpOnly" y para obtener el valor se tiene que abrir el panel de 'DevTools' del navegador pulsando CTRL+SHIFT+I e ir a la pestaña "Application" (Chrome) o "Storage" (Firefox), allí ir al apartado "Cookies" y copiar el valor de la cookie que se llama "sess".
 
 - Ejemplo, editar todos los menajes de 'myuser' omitiendo el principal de los hilos:
+    
     ```./MVMassEdit.py -u myuser --omit-first```
 
-## Roadmap
+Tras ejecutarse el script se generarán dos archivos:
+- mvmassedit.log - Contendrá un log con todo lo que ha sucedido en la última ejecución
+- fails.json - Contendrá todas las url's de los posts que no han podido ser editados
 
-- Mejorar el uso de multi-requests para tener en cuenta las ventanas de tiempo.
-- Ver el motivo por el cual no edita algunos mensajes correctamente. Todo apunta a que es por la protección anti-flood que usa mediavida.
+
+## Hoja de Ruta / Errores conocidos
+
+- Añadir una opción para 'cortar' la lista de posts, esto es menos agresivo que el filtro por fechas y permite una mínima personalización aproximada de lo que se va a editar.
+- Añadir una opción para editar solo los mensajes que contengan una o varias palabras clave.
